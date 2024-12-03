508809c9fc2ae7b5541795e7cb50e9dd039c2fe8
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-12-03 02:18:57 -0700

    Update code.js

--- code.js
+++ code.js
@@ -8,6 +8,25 @@ function tsp_hk(distance_matrix) {
     //base case
     table[1][0] = 0; 
     
+    //start iterations
+
+    for(let visitedSubset =1; visitedSubset < (1 << n); visitedSubset++){
+
+        for (let curCity = 1; curCity < n; curCity++){
+            //used replit ai tool to help get the sytax right in order to skip if city isnt in the subset
+            if(!(visitedSubset & (1 << curCity))) continue;
+
+            for( let nextCity=0; nextCity < n; nextCity++){
+                //used the replit example above to write this skip too(don't know if I need to mark this but I will)
+                if ((visitedSubset & (1 << nextCity)) || curCity === nextCity) continue;
+
+                let nextVisitedSubset = visitedSubset | (curCity ===nextCity);
+
+                table[nextVisitedSubset][nextCity], table[visitedSubset][curCity] + distance_matrix[curCity][nextCity];
+            }
+        }
+    }
+
 
 
 
@@ -18,3 +37,4 @@ function tsp_hk(distance_matrix) {
 
     return -1;
 }
+
