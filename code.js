636b5f482d109e26d2fcad4b2f0a4fd49d54f2eb
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-12-03 02:03:37 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 function tsp_hk(distance_matrix) {
+
+    let n = distance_matrix.length;
+
+    //asked replit ai tool for help with the syntax for creating the the table I wanted
+    let table = Array.from({length: 1 << n},() => Array(n).fill(Infinity ));
+    
+    //base case
+    table[1][0] = 0; 
+    
+
+
+
+
+
+
+
+
     return -1;
 }
